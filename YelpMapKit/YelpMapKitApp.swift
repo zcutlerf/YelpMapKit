5f9ba68c2f144b5879740d445627d787d947ede6
Zoe Cutler <zcutlerf@gmail.com>
2023-05-01 14:55:37 -0400

    Initial Commit

---
+++ YelpMapKit/YelpMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpMapKitApp.swift
+//  YelpMapKit
+//
+//  Created by Zoe Cutler on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct YelpMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
